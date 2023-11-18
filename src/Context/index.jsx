0531612a41d5bfc03672184e0b884584177b2af5
Sandeep Maurya <mauryasandip4you@gmail.com>
2023-11-18 11:33:44 +0530

    Weather_Application

---
+++ src/Context/index.jsx
@@ -0,0 +1,66 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import axios from 'axios'
+
+const StateContext = createContext()
+
+// eslint-disable-next-line react/prop-types
+export const StateContextProvider = ({ children }) => {
+    const [weather, setWeather] = useState({})
+    const [values, setValues] = useState([])
+    const [place, setPlace] = useState('Jaipur')
+    const [thisLocation, setLocation] = useState('')
+
+    // fetch api
+    const fetchWeather = async () => {
+        const options = {
+            method: 'GET',
+            url: 'https://api.openweathermap.org/data/2.5',
+            params: {
+                aggregateHours: '24',
+                location: place,
+                contentType: 'json',
+                unitGroup: 'metric',
+                shortColumnNames: 0,
+            },
+            headers: {
+                'REACT_APP_API_KEY =': 'dc1afbcb4200da61c8b755cd144588c7', 
+                'open-weather': 'https://openweathermap.org/img/w'
+            }
+        }
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data)
+            const thisData = Object.values(response.data.locations)[0]
+            setLocation(thisData.address)
+            setValues(thisData.values)
+            setWeather(thisData.values[0])
+        } catch (e) {
+            console.error(e);
+            // if the api throws error.
+            alert('This place does not exist')
+        }
+    }
+
+    useEffect(() => {
+        fetchWeather()
+    }, [place])
+
+    useEffect(() => {
+        console.log(values)
+    }, [values])
+
+    return (
+        <StateContext.Provider value={{
+            weather,
+            setPlace,
+            values,
+            thisLocation,
+            place
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
